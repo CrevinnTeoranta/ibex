bbba0998e46952a5627d7c2114e060838b4dede8
Sean Dexter <sean_dexter@crevinn.com>
2021-02-16 12:03:37 +0000

    making custom includes as generic as possible

--- examples/sw/simple_system/hello_test/hello_test.c
+++ examples/sw/simple_system/hello_test/hello_test.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "fast-vdma.h"
+#include "custom-includes.h"
 #include "simple_system_common.h"
 
 int main(int argc, char **argv) {
